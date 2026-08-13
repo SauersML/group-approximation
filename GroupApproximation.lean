261d807367d85113f40c6fe5d37fc0ad6278b5a6
Sauers <>
2026-08-13 02:15:41 -0500

    Wire literal base relations and remove the dead SOS layer

--- GroupApproximation.lean
+++ GroupApproximation.lean
@@ -16,6 +16,7 @@ import GroupApproximation.Monsters.CyclicBaseCalibration
 import GroupApproximation.Monsters.LiteralCyclicCalibration
 import GroupApproximation.Sofic.LiteralNonMFLinearWitness
 import GroupApproximation.Sofic.LiteralBaseAffineQuotient
+import GroupApproximation.Sofic.LiteralBaseRelations
 import GroupApproximation.Sofic.LiteralFiniteDimensionalObstruction
 import GroupApproximation.Sofic.LiteralUniversalHorn
 import GroupApproximation.Sofic.LiteralPresentationRadius
