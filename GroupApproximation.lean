9ce990d0c1770be0cc9a765146b65b076baee7f0
Sauers <>
2026-08-13 01:28:21 -0500

    Wire intrinsic defect bridge into manuscript

--- GroupApproximation.lean
+++ GroupApproximation.lean
@@ -14,6 +14,7 @@ import GroupApproximation.Kazhdan.RationalGroupRingCertificate
 import GroupApproximation.Kazhdan.LiteralBaseSOS
 import GroupApproximation.Kazhdan.LiteralRelatorReduction
 import GroupApproximation.Kazhdan.FixedSpaceStabilizer
+import GroupApproximation.Kazhdan.FixedSpaceDefect
 import GroupApproximation.Sofic.AdjointMatrix
 import GroupApproximation.Sofic.ProjectionRankFlip
 import GroupApproximation.Sofic.SpectralCapture
