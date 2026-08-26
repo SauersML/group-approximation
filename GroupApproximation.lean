8bbc55ee4a793f5999a6500df8f5316693248427
Sauers <>
2026-08-26 15:48:12 -0500

    Add benign infinite code formalization

--- GroupApproximation.lean
+++ GroupApproximation.lean
@@ -95,6 +95,7 @@ import GroupApproximation.Computability.AmalgamCode
 import GroupApproximation.Computability.AmalgamCodeSemantics
 import GroupApproximation.Computability.AmalgamCodePushout
 import GroupApproximation.Computability.BenignMapEmbCode
+import GroupApproximation.Computability.BenignInfCode
 import GroupApproximation.Computability.BenignMapEmbRopeInput
 import GroupApproximation.Computability.DirectProductCodeSemantics
 import GroupApproximation.Computability.AdianRabinVariantTransform
