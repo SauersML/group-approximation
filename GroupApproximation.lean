aa99bb7072d1d3b013ab70acb10d3b49a159fabb
Sauers <>
2026-08-26 12:34:06 -0500

    Identify the effective rope compiler layers

--- GroupApproximation.lean
+++ GroupApproximation.lean
@@ -1851,6 +1851,7 @@ import GroupApproximation.Higman.MikhailovaFiberProduct
 import GroupApproximation.Higman.MikhailovaGraphProductWitness
 import GroupApproximation.Higman.MikhailovaRankThreeWitness
 import GroupApproximation.Higman.MikhailovaRopeCode
+import GroupApproximation.Higman.MikhailovaRopeCodeSemantics
 import GroupApproximation.Higman.MikhailovaRopeCompiler
 import GroupApproximation.Higman.FreeLampFinitePresentation
 import GroupApproximation.Higman.PairedFoldKernel
