019e2816548dd182b3bd4bb7d12f7fef8dfeeb89
Sauers <>
2026-08-31 14:38:53 -0500

    Port cone simplex internal face identity

--- GroupApproximation/ThirdParty/HamSandwich/SphereOddDegree/AlgebraicTopology/BarycentricSubdivisionConeSimplexFaceSucc.lean
+++ GroupApproximation/ThirdParty/HamSandwich/SphereOddDegree/AlgebraicTopology/BarycentricSubdivisionConeSimplexFaceSucc.lean
@@ -15,9 +15,13 @@ theorem coneSimplex_face_succ (n k : ℕ) (v : Delta n)
     (σ : singularSimplices (TopCat.of (Delta n)) (k + 1)) (j : Fin (k + 1 + 1)) :
     AlexanderWhitney.faceSimplex (TopCat.of (Delta n)) (k + 1) j.succ (coneSimplex n (k + 1) v σ)
       = coneSimplex n k v (AlexanderWhitney.faceSimplex (TopCat.of (Delta n)) k j σ) := by
-  apply singularSimplices_ext; simp [faceSimplex_continuousMap, coneSimplex_continuousMap];
-  ext y; simp [affineConeContinuousMap_apply, ContinuousMap.comp_apply];
-  convert congr_arg ( fun f => f ‹_› ) ( congrFun ( cone_face_succ v ( singularSimplexAsContinuousMap { carrier := ↑(Delta n), str := instTopologicalSpaceSubtype } (k + 1) σ ) j ) y ) using 1
+  apply singularSimplices_ext
+  rw [faceSimplex_continuousMap, coneSimplex_continuousMap,
+    coneSimplex_continuousMap, faceSimplex_continuousMap]
+  apply ContinuousMap.ext
+  intro y
+  exact congrFun
+    (cone_face_succ v
+      (singularSimplexAsContinuousMap (TopCat.of (Delta n)) (k + 1) σ) j) y
 end AffineBarycentricSubdivision
 end GroupApproximation.ThirdParty.HamSandwich.SphereOddDegree
-
