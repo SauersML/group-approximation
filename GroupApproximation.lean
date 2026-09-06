9204c38ff06bcf37c303809524f42d5968591682
Sauers <>
2026-09-06 00:56:39 -0400

    LIX root: fix malformed import line from batch twenty-seven (zsh no-split left a space after the dot)

--- GroupApproximation.lean
+++ GroupApproximation.lean
@@ -3608,7 +3608,7 @@ import GroupApproximation.CharClass.CartanMidFourChain
 import GroupApproximation.CharClass.BundleGysinPieces
 import GroupApproximation.CharClass.ProjectiveSpaceGenHyp
 import GroupApproximation.CharClass.LerayHirschDiagCover
-import GroupApproximation. CharClass.LerayHirschPlusOne
+import GroupApproximation.CharClass.LerayHirschPlusOne
 
 /-!
 # An unconditional construction of a finitely presented nonsofic group
